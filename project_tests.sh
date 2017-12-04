7d279b3498eb3dd5e4a759c2118f3f43124736df
Sean Wiseman <seanwiseman2012@gmail.com>
2017-12-04 16:27:21 +0000

    Actually run coverage

--- project_tests.sh
+++ project_tests.sh
@@ -14,6 +14,6 @@ pip install coveralls
 
 pylint --reports=n elife_bus_sdk
 flake8 elife_bus_sdk/ test/
-python -m pytest --junitxml=build/pytest.xml
+coverage run -m pytest --junitxml=build/pytest.xml
 
 COVERALLS_REPO_TOKEN=$(cat /etc/coveralls/tokens/elife-bus-sdk) coveralls
