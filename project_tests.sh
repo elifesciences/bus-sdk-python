6f23da8a462c1e4a0b3b355df545d2f06f65724d
LSH <lsh-0@users.noreply.github.com>
2020-02-11 12:37:52 +1030

    changes py35 environment to just py3 (#9)

--- project_tests.sh
+++ project_tests.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-if [ ! -e "venv/bin/python3.5" ]; then
-    echo "could not find venv/bin/python3.5, recreating venv"
+if [ ! -e "venv/bin/python3" ]; then
+    echo "could not find venv/bin/python3, recreating venv"
     rm -rf venv
-    virtualenv --python=python3.5 venv
+    virtualenv --python=python3 venv
 fi
 
 source venv/bin/activate
