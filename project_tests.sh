b7edae199f87c406a38c2869d5c18ed905244da2
Luke <lsh-0@users.noreply.github.com>
2021-12-14 14:25:39 +1030

    adds install.sh, adds 'coverage' dependency, updates Jenkinsfile

--- project_tests.sh
+++ project_tests.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
 set -e
-
-if [ ! -e "venv/bin/python3" ]; then
-    echo "could not find venv/bin/python3, recreating venv"
-    rm -rf venv
-    virtualenv --python=python3 venv
-fi
-
-source venv/bin/activate
-
-pip install -r requirements.txt
-pip install -r requirements-dev.txt
-
+. install.sh
 coverage run -m pytest --junitxml=build/pytest.xml
